4dd0cb773f10fe05d6e24f8347f7f90e6797ea2c
Eric <ericroble2.0@gmail.com>
2026-02-11 11:14:31 +0100

    actualizado

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
-import Users from "../pages/Users";
-import Para_ti from "../pages/Para_ti";
+
 
 import NotFound from "../pages/NotFound";
 import Comunidades from "../pages/Comunidades";
@@ -14,18 +13,21 @@ import Terminos from "./pagesFooter/terminos";
 function AppRouter() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/Usuario" element={<Users />} />
-        <Route path="/Para_ti" element={<Para_ti />} />
-        <Route path="/Comunidades" element={<Comunidades />} />
-        <Route path="/comunidad/:id" element={<DetallesComunidades />} />
-        <Route path="/terminos" element={<Terminos />} />
+      <div className="app-container">
+        <div className="content-wrap"> 
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/comunidades" element={<Comunidades />} />
+            <Route path="/comunidad/:id" element={<DetallesComunidades />} />
+            <Route path="/terminos" element={<Terminos />} />
             <Route path="/privacidad" element={<Privacidad />} />
             <Route path="/ayuda" element={<Ayuda />} />
             <Route path="/contacto" element={<Contacto />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+      <Footer />
     </BrowserRouter>
   );
 }
