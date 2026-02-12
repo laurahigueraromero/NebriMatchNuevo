87b4a7c3db91c6b1e845281bae55819090655416
Laura Higuera <laurahigueraromero@gmail.com>
2026-02-12 12:35:38 +0100

    registro arreglado

--- frontend/src/pagesFooter/Ayuda.jsx
+++ frontend/src/pagesFooter/Ayuda.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
-import Header from "../components/Header";
+import Header from "../componentes/Header";
+
 
 function Ayuda() {
   const navigate = useNavigate();
