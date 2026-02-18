5d8193f4907df58c094622cfca7012b3217c0ca6
Eric <ericroble2.0@gmail.com>
2026-02-18 14:25:11 +0100

    Cambios

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 
-
 import NotFound from "../pages/NotFound";
 import Comunidades from "../pages/Comunidades";
 import DetallesComunidades from "../pages/DetallesComunidades";
@@ -11,30 +10,27 @@ import Privacidad from "../pagesFooter/Privacidad";
 import Terminos from "../pagesFooter/terminos";
 import Footer from "../componentes/Footer";
 
-import MiPerfil from "../pages/MiPerfil"; 
+import MiPerfil from "../pages/MiPerfil";
 import Home from "../pages/Home";
 import ParaTi from "../pages/ParaTi";
 import Chats from "../pages/Chats";
 
 function AppRouter() {
   return (
-
     <BrowserRouter>
       <div className="app-container">
-        <div className="content-wrap"> 
-         <Routes>
-        <Route path="/" element={<Home />} />
+        <div className="content-wrap">
+          <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
-          
-<Route path="/miPerfil" element={<MiPerfil />} />
-<Route path="/miPerfil/editarPerfil" element={<MiPerfil />} />
 
+            <Route path="/miPerfil" element={<MiPerfil />} />
+            <Route path="/miPerfil/editarPerfil" element={<MiPerfil />} />
 
             <Route path="/comunidades" element={<Comunidades />} />
             <Route path="/parati" element={<ParaTi />} />
             <Route path="/chats" element={<Chats />} />
-            
-           
+
             <Route path="/comunidad/:id" element={<DetallesComunidades />} />
             <Route path="/terminos" element={<Terminos />} />
             <Route path="/privacidad" element={<Privacidad />} />
@@ -47,7 +43,6 @@ function AppRouter() {
       <Footer />
     </BrowserRouter>
   );
-  
 }
 
 export default AppRouter;
