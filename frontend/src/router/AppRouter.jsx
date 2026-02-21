1735e79da8faa3cb878b4ddaf52a68c82a2e7002
Eric <ericroble2.0@gmail.com>
2026-02-21 20:20:28 +0100

    Arreglado el footer

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,6 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Login from "../pages/Login";
-
 import NotFound from "../pages/NotFound";
 import Comunidades from "../pages/Comunidades";
 import DetallesComunidades from "../pages/DetallesComunidades";
@@ -9,38 +8,48 @@ import Contacto from "../pagesFooter/Contacto";
 import Privacidad from "../pagesFooter/Privacidad";
 import Terminos from "../pagesFooter/terminos";
 import Footer from "../componentes/Footer";
-
 import MiPerfil from "../pages/MiPerfil";
 import Home from "../pages/Home";
 import ParaTi from "../pages/ParaTi";
 import Chats from "../pages/Chats";
 
+// 1. Creamos un componente interno para que useLocation esté DENTRO del Router
+function AppContent() {
+  const location = useLocation();
+  
+  // Aquí definimos las rutas donde NO queremos el Footer (Home y Login)
+  const ocultarFooter = ["/login", "/"].includes(location.pathname);
+
+  return (
+    <div className="app-container">
+      <div className="content-wrap">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/miPerfil" element={<MiPerfil />} />
+          <Route path="/miPerfil/editarPerfil" element={<MiPerfil />} />
+          <Route path="/comunidades" element={<Comunidades />} />
+          <Route path="/parati" element={<ParaTi />} />
+          <Route path="/chats" element={<Chats />} />
+          <Route path="/comunidad/:id" element={<DetallesComunidades />} />
+          <Route path="/terminos" element={<Terminos />} />
+          <Route path="/privacidad" element={<Privacidad />} />
+          <Route path="/ayuda" element={<Ayuda />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      {/* El Footer ahora sí sabe en qué página está */}
+      {!ocultarFooter && <Footer />}
+    </div>
+  );
+}
+
+// 2. El componente principal que exportas solo envuelve todo con el BrowserRouter
 function AppRouter() {
   return (
     <BrowserRouter>
-      <div className="app-container">
-        <div className="content-wrap">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-
-            <Route path="/miPerfil" element={<MiPerfil />} />
-            <Route path="/miPerfil/editarPerfil" element={<MiPerfil />} />
-
-            <Route path="/comunidades" element={<Comunidades />} />
-            <Route path="/parati" element={<ParaTi />} />
-            <Route path="/chats" element={<Chats />} />
-
-            <Route path="/comunidad/:id" element={<DetallesComunidades />} />
-            <Route path="/terminos" element={<Terminos />} />
-            <Route path="/privacidad" element={<Privacidad />} />
-            <Route path="/ayuda" element={<Ayuda />} />
-            <Route path="/contacto" element={<Contacto />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </div>
-      <Footer />
+      <AppContent />
     </BrowserRouter>
   );
 }
