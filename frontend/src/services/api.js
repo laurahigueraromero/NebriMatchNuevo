e5807a945fb84441edabfa8a058b075c2a5e3256
Laura Higuera <laurahigueraromero@gmail.com>
2026-02-21 21:36:05 +0100

    env example

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,6 @@
 // const API_URL = 'http://localhost:4004';
 const API_URL = import.meta.env.VITE_API_URL;
+
 // ============ AUTENTICACIÓN ============
 
 export const login = async (email, password) => {
